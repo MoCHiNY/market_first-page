6f66fca47cd9c9a464777b4d3fa238d15332de49
MoCHiNY <terebill2@gmail.com>
2021-04-05 14:16:10 +0300

    moduls-beta

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,85 @@
+
 $(function () {
+$('.related__slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: false,
+});
+
+$('.product-tabs__top-item').on('click',function (e) {
+  e.preventDefault();
+  $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+  $(this).addClass('product-tabs__top-item--active');
+
+  $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+  $($(this).attr('href')).addClass('product-tabs__content-item--active');
+});
+
+$('.product-details__slider-1').slick({
+   slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.product-details__slider-2',
+  vertical:true,
+  arrows:false,
+  focusOnSelect:true
+});
+
+$('.product-details__slider-2').slick({
+  asNavFor: '.product-details__slider-1',
+  arrows:false,
+  fade:true,
+});
+
+
+
+
+$('.contact-filter__btn').on('click',function (){
+  $('.contact-filter__btn').removeClass('contact-filter__btn-active');
+  $(this).addClass('contact-filter__btn-active');
+});
+
+$('.contact-filter__btn-grid').on('click',function () {
+$('.products-one__item').removeClass('products-one__item--list');
+$('.pagination').removeClass('pagination__list--mod');
+});
+
+$('.contact-filter__btn-list').on('click',function () {
+$('.products-one__item').addClass('products-one__item--list');
+$('.pagination').addClass('pagination__list--mod');
+});
+
+
+
+$('.contact-filter__select, .product-details__input').styler({
+});
+
+$(".filter-recent__stars--mod").rateYo({
+ starWidth: "20px",
+ ratedFill: "#ffcc00",
+ rating: "80%",
+ spacing: "10px"
+});
+
+$(".filter-recent__stars").rateYo({
+ starWidth: "16px",
+ ratedFill: "#ffcc00",
+ rating: "80%",
+ precision: 0
+});
+
+
+$(".filter-price__input").ionRangeSlider({ 
+      type: "double",
+      prefix: "$",
+      onStart: function (data) {
+  $('.filter-price__from').text(data.from);
+  $('.filter-price__to').text(data.to);
+      },
+        onChange: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+        },
+    });
   
   $('.slider-cooperation__list').slick({
   arrows:false,
@@ -36,3 +117,5 @@ var mixer1 = mixitup('.products-one__content', {
 
 
 });
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
 const {src, dest, watch, parallel, series} = require('gulp'); 
 //parallel одночасно запускає дві дії
 //series запускає почерзі файли
-const scss         = require('gulp-sass');
-const concat       = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const browserSync  = require('browser-sync').create();
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
+const scss            = require('gulp-sass');
+const concat          = require('gulp-concat');
+const autoprefixer    = require('gulp-autoprefixer');
+const uglify          = require('gulp-uglify');
+const rename         = require('gulp-rename');
+const nunjucksrender  = require('gulp-nunjucks-render');
+const browserSync     = require('browser-sync').create();
+const imagemin        = require('gulp-imagemin');
+const del             = require('del');
+
+function nunjucks () {
+  return src('app/*.njk')
+  .pipe(nunjucksrender())
+  .pipe(dest('app'))
+  .pipe(browserSync.stream())
+}
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src('app/scss/*.scss')
     .pipe(scss({
       outputStyle: 'compressed'// стиль кода який отримаємо(стиснутий)
     }))
-    .pipe(concat('style.min.css'))
+    .pipe(rename({
+      suffix : '.min'
+    }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
       grid: true
@@ -29,6 +40,9 @@ function scripts() {
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/mixitup/dist/mixitup.js',
       'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -74,7 +88,9 @@ function cleanDist() {
 }// npm i --sev-dev del плагин для очистки
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/scss/*.scss'], styles);
+  watch(['app/module/**/*.scss'], styles);
+  watch(['app/*.njk'], nunjucks);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);// слідкує за всіма файлами окрім !
   watch(['app/*.html']).on('change', browserSync.reload);// перезагрузка сторінки 
 
@@ -86,5 +102,6 @@ exports.browsersync = browsersync;
 exports.images = images;
 exports.watching = watching;
 exports.cleanDist = cleanDist;
+exports.nunjucks = nunjucks;
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(nunjucks,styles, scripts, browsersync, watching);
